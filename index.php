da1c7f895c48d04b90210da258334352e83702ff
sourabhjn <sourabh.jain69@gmail.com>
2017-02-15 16:28:22 +0530

    Its done sourabh

---
+++ index.php
@@ -0,0 +1,14 @@
+<!<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Demo</title>
+	<link rel="stylesheet" href="css/style.csss">
+</head>
+<body>
+	<div class="demo1">
+		hello
+	</div>
+</body>
+</html>
